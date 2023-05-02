01f4d9629b2d47741270d1ea66a6d8ae4fb21427
Leonardo Sibela <leonardo_sibela@hotmail.com>
2023-05-02 16:08:28 -0300

    Add input and button

--- app/src/main/java/com/honkgoose/composestaticlist/MainActivity.kt
+++ app/src/main/java/com/honkgoose/composestaticlist/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.honkgoose.composestaticlist
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.honkgoose.composestaticlist.ui.theme.ComposeStaticListTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +19,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeStaticListTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TodoList()
                 }
             }
         }
@@ -30,14 +31,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TodoList() {
+    var name by remember { mutableStateOf("") }
+    val names by remember { mutableStateOf(emptyList<String>()) }
+    val context = LocalContext.current
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(16.dp, 16.dp, 16.dp, 16.dp)
+                    .weight(1f),
+                onValueChange = { text -> name = text },
+                value = name
+            )
+            Button(
+                modifier = Modifier
+                    .padding(0.dp, 16.dp, 16.dp, 16.dp)
+                    .wrapContentSize(),
+                onClick = {
+                    if (name.isNotBlank()) {
+                        names + name
+                        Toast.makeText(context, name, Toast.LENGTH_SHORT).show()
+                        name = ""
+                    }
+                }
+            ) {
+                Text(text = "Add")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStaticListTheme {
-        Greeting("Android")
+        TodoList()
     }
 }
